a26cf5136371f8991463b4f2ba753dcd129af6e8
Laura Collins <laurasmile@gmail.com>
2024-02-12 00:41:04 -0500

    added the bucket name variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
     default = "us-east-1"
 }
 
+variable "bucket_name" {
+    description = "Name of the bucket"
+    type = string
+    default = "web-hosting-bucket-northwest"
+}
