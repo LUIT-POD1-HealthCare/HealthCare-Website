455bbb65b1034d2ca3a47aa244cdce47a47ee606
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2024-02-20 07:55:41 -0600

    Updated tests to trigger on PULL_REQUEST_UPDATED and PULL_REQUEST_REOPENED

--- CI_Pipeline_Dev/main.tf
+++ CI_Pipeline_Dev/main.tf
@@ -217,12 +217,20 @@ resource "aws_codebuild_webhook" "webhook" {
 
   filter_group {
     filter {
+      type    = "BASE_REF"
+      pattern = "refs/heads/${var.github_branch}"
+    }
+    filter {
       type    = "EVENT"
       pattern = "PULL_REQUEST_CREATED"
     }
     filter {
-      type    = "BASE_REF"
-      pattern = "refs/heads/${var.github_branch}"
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_UPDATED"
+    }
+    filter {
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_REOPENED"
     }
   }
 }
@@ -248,7 +256,6 @@ resource "aws_codebuild_project" "filter" {
   source {
     type                = "CODEPIPELINE"
     buildspec           = "CI_Pipeline_Dev/files/filter_buildspec.yml"
-    report_build_status = true
   }
   artifacts {
     type = "CODEPIPELINE"
